1ab77ed2614f2458ae6ca435839b7a1e5a1427d6
Solant <runner62v6@gmail.com>
2017-04-20 19:24:06 +0300

    Book saving to database implemented

--- client/js/redux/saga/index.saga.js
+++ client/js/redux/saga/index.saga.js
@@ -1,10 +1,18 @@
-import { delay, takeEvery } from 'redux-saga';
+import { takeEvery } from 'redux-saga';
 import { put } from 'redux-saga/effects';
+import isomorphicFetch from 'isomorphic-fetch';
 import { createBook, createBookSuccess } from '../modules/examples.module';
 
 function* createBookSaga(action) {
-  yield delay(1000);
-  yield put(createBookSuccess(action.payload));
+  const data = yield isomorphicFetch('http://localhost:3000/api/book/add', {
+    method: 'POST',
+    body: JSON.stringify(action.payload),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(response => response.json());
+  data.id = data._id;
+  yield put(createBookSuccess(data));
 }
 
 function* watchCreateBook() {
